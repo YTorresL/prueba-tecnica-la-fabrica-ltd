690dcae71350ea51d1fb83922431ffd70012af34
Yalith Torres <yalithdev@gmail.com>
2024-12-15 00:24:19 -0400

    Corrección de css

--- app/page.js
+++ app/page.js
@@ -1,3 +1,21 @@
+import { Footer } from '@/components/layout/footer'
+import { Header } from '@/components/layout/header'
+import { Hero } from '@/sections/homepage/hero'
+import { Qr } from '@/sections/homepage/qr'
+import { SelectType } from '@/sections/qr/select'
+
 export default function Home() {
-  return <></>
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <section id="qr">
+          <Qr />
+          <SelectType />
+        </section>
+      </main>
+      <Footer />
+    </>
+  )
 }
