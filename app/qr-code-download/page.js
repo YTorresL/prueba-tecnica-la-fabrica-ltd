88730f27672bc1ce296b6e6d882f84c6553eabd9
Yalith Torres <yalithdev@gmail.com>
2024-12-15 12:54:09 -0400

    Correccion de pantalla download.

--- app/qr-code-download/page.js
+++ app/qr-code-download/page.js
@@ -6,11 +6,9 @@ import { Header } from '@/components/qr/header'
 export default function Page() {
   return (
     <>
-      <Header />
       <section className="min-h-screen custom-width px-4 py-[80px]">
         <h1 className="text-3xl font-bold mt-10">QR Generator</h1>
       </section>
-      <Footer />
     </>
   )
 }
