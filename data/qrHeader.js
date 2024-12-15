735df4c8b20b63ae33914571249a408bb56df4ff
Yalith Torres <yalithdev@gmail.com>
2024-12-15 00:22:42 -0400

    Datos de los headers

---
+++ data/qrHeader.js
@@ -0,0 +1,22 @@
+export const Stepper = [
+  {
+    id: 1,
+    title: 'Select QR type',
+    link: '/qr-code-generator'
+  },
+  {
+    id: 2,
+    title: 'Add content',
+    link: '/qr-code-generator/'
+  },
+  {
+    id: 3,
+    title: 'Design QR code',
+    link: '/qr-code-design'
+  },
+  {
+    id: 4,
+    title: 'Download QR code',
+    link: '/qr-code-download'
+  }
+]
---
+++ data/qrTypes.js
@@ -0,0 +1,26 @@
+export const Types = [
+  {
+    id: 1,
+    title: 'Website',
+    description: 'Link to any website URL',
+    phoneContent: 'Website QR Example',
+    link: '/qr-code-generator/website',
+    slug: 'website'
+  },
+  {
+    id: 2,
+    title: 'PDF',
+    description: 'Show a PDF',
+    phoneContent: 'PDF QR Example',
+    link: '/qr-code-generator/pdf',
+    slug: 'pdf'
+  },
+  {
+    id: 3,
+    title: 'vCard',
+    description: 'Share your digital business card',
+    phoneContent: 'vCard QR Example',
+    link: '/qr-code-generator/vcard',
+    slug: 'vcard'
+  }
+]
