c6396e3ecdb5779455062b8e99b03066e8bdeeaa
Yalith Torres <yalithdev@gmail.com>
2024-12-16 04:29:53 -0400

    Footer, eliminacion de tipos.

--- data/qrTypes.js
+++ data/qrTypes.js
@@ -1,14 +1,5 @@
 export const Types = [
   {
-    id: 1,
-    title: 'Website',
-    description: 'Link to any website URL',
-    phoneContent: 'Website QR Example',
-    link: '/qr-code-generator/website',
-    slug: 'website',
-    icon: '🌐'
-  },
-  {
     id: 2,
     title: 'PDF',
     description: 'Show a PDF',
@@ -16,14 +7,5 @@ export const Types = [
     link: '/qr-code-generator/pdf',
     slug: 'pdf',
     icon: '📄'
-  },
-  {
-    id: 3,
-    title: 'vCard',
-    description: 'Share your digital business card',
-    phoneContent: 'vCard QR Example',
-    link: '/qr-code-generator/vcard',
-    slug: 'vcard',
-    icon: '📇'
   }
 ]
