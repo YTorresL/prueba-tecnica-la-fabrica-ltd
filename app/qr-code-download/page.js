29e9cf9f8761b5b939436ba82a80416521c34831
Yalith Torres <yalithdev@gmail.com>
2024-12-16 05:49:30 -0400

    Seccion de descargas o muestra de qr.

--- app/qr-code-download/page.js
+++ app/qr-code-download/page.js
@@ -1,7 +1,12 @@
 'use client'
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+import { useForm } from '@/hooks/useForm'
+import { Download } from '@/sections/qr/download'
+import Link from 'next/link'
 
 export default function Page() {
+  const { form, url } = useForm()
   const router = useRouter()
 
   useEffect(() => {
@@ -12,8 +17,23 @@ export default function Page() {
 
   return (
     <>
-      <section className="min-h-screen custom-width px-4 py-[80px]">
-        <h1 className="text-3xl font-bold mt-10">QR Generator</h1>
+      <section className="custom-width space-y-5 md:space-y-7 w-full relative p-4 md:p-6">
+        <div className="w-full">
+          <div className="flex items-center">
+            <Link className="w-10" href="/qr-code-generator">
+              <img
+                alt="QR KOALA Logo"
+                src="/qr_koala_logo.webp"
+                width="28"
+                height="28"
+              />
+            </Link>
+            <p className="font-black text-xl mt-1">QR KOALA</p>
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <Download />
+        </div>
       </section>
     </>
   )
