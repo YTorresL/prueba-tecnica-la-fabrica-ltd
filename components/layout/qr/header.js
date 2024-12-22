c384b3123bc1b0be7ec1f15ed5f39a0cb67107cb
Yalith Torres <yalithdev@gmail.com>
2024-12-22 00:54:47 -0400

    Favicon

--- components/layout/qr/header.js
+++ components/layout/qr/header.js
@@ -1,71 +1,47 @@
 import { Stepper } from '@/data/qrHeader'
 import { useForm } from '@/hooks/useForm'
-import Image from 'next/image'
 import Link from 'next/link'
 
 export function Header() {
   const { step, form, setStep } = useForm()
 
-  // Buscar el paso actual y obtener su información
   const currentStep = Stepper.find((s) => s.id === step)
 
   return (
     <header className="fixed top-0 w-full flex justify-between items-center border-b border-gray-200 h-[4rem] px-5 md:px-10 backdrop-blur-lg z-10 bg-white/70 backdrop-saturate-150">
       <Link className="w-10" href="/">
-        <Image
+        <img
           alt="QR KOALA Logo"
-          width={28}
-          height={28}
+          width="28"
+          height="28"
           src="/qr_koala_logo.webp"
         />
       </Link>
       <ol className="flex items-center w-full p-3 space-x-2 justify-center text-sm font-medium text-center sm:p-4 sm:space-x-4 rtl:space-x-reverse">
         {Stepper.map((step) => (
-          <div key={step.id} className="flex items-center">
-            <Link
-              className={`flex items-center ${
-                currentStep.id >= step.id
-                  ? 'text-black'
-                  : 'text-gray-300 pointer-events-none'
-              }`}
-              href={
-                form.type.length && step.id === 2
-                  ? step.link + form.type
-                  : step.link
-              }
-              onClick={() => setStep(step.id)}
+          <Link
+            className={`flex items-center ${
+              currentStep.id >= step.id
+                ? 'text-black'
+                : 'text-gray-300 pointer-events-none'
+            }`}
+            key={step.id}
+            href={
+              form.type.length && step.id === 2
+                ? step.link + form.type
+                : step.link
+            }
+            onClick={() => setStep(step.id)}
+          >
+            <span
+              className={`flex items-center justify-center w-5 h-5 text-xs me-2 ${
+                currentStep.id >= step.id ? 'bg-black' : 'bg-gray-300'
+              } rounded-full shrink-0 text-white`}
             >
-              <span
-                className={`flex items-center justify-center w-5 h-5 text-xs me-2 ${
-                  currentStep.id >= step.id ? 'bg-black' : 'bg-gray-300'
-                } rounded-full shrink-0 text-white`}
-              >
-                {step.id === 1 && currentStep != 1 ? '✓' : step.id}
-              </span>
-              {step.title}
-            </Link>
-            {step.id < Stepper.length && (
-              <div className="w-14 flex items-center ml-3">
-                <div
-                  className={`h-px w-full flex-1 ${
-                    currentStep.id > step.id ? 'bg-black' : 'bg-gray-300'
-                  }`}
-                ></div>
-                <div
-                  className="triangle-right"
-                  style={
-                    currentStep.id > step.id
-                      ? {
-                          borderLeftColor: '#000'
-                        }
-                      : {
-                          borderLeftColor: '#ccc'
-                        }
-                  }
-                ></div>
-              </div>
-            )}
-          </div>
+              {step.id}
+            </span>
+            {step.title}
+          </Link>
         ))}
       </ol>
     </header>
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
-  images: {
-    unoptimized: true
-  }
+  distDir: 'build'
 }
 
 export default nextConfig
